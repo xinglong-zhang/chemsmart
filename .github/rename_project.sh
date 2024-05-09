2fe1ff6c743d892827863cb5fb92f3c03f30f72a
xinglong-zhang <xinglong-zhang@users.noreply.github.com>
2024-05-09 08:31:36 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="xinglong-zhang"
+original_name="chemsmart"
+original_urlname="chemsmart"
+original_description="Awesome chemsmart created by xinglong-zhang"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv chemsmart $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
