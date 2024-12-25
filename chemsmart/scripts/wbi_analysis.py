5160be4b3e7e01fd7a137a6ff34aa77f3dd6d216
Xinglong Zhang <xinglong.zhang@chem.ox.ac.uk>
2024-12-25 12:21:06 +0800

    added script to obtain natural charges from NBO calcs

---
+++ chemsmart/scripts/wbi_analysis.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+import logging
+import os
+import click
+from chemsmart.utils.utils import create_logger
+from chemsmart.io.gaussian.output import Gaussian16WBIOutput
+from chemsmart.utils.utils import (
+    get_value_by_number,
+    get_key_by_value_and_number,
+)
+
+logger = logging.getLogger(__name__)
+os.environ["OMP_NUM_THREADS"] = "1"
+
+
+@click.command()
+@click.option(
+    "-f",
+    "--filename",
+    required=True,
+    default=None,
+    type=str,
+    help="Gaussian output file for Wiberg Bond Index analysis.",
+)
+@click.option(
+    "-n",
+    "--numbers",
+    default=None,
+    type=int,
+    multiple=True,
+    help="Atom numbers from which to obtain data from Wiberg Bond Index output file. 1-indexed.",
+)
+def entry_point(filename, numbers):
+    create_logger()
+    outputfile = Gaussian16WBIOutput(filename=filename)
+
+    # npa = outputfile.natural_population_analysis
+    # logger.info("\nNatural Population Analysis:")
+    # for npkey, npvalue in npa.items():
+    #     natural_charge = npvalue["natural_charge"]
+    #     logger.info(f"{npkey}  :  {natural_charge:.3f}")
+    # logger.info("\n")
+    # same as above
+    natural_charges = outputfile.natural_charges
+    logger.info("\nNatural Charges:")
+    for nkey, nvalue in natural_charges.items():
+        logger.info(f"{nkey:<6}  :  {nvalue:>8.3f}")
+    logger.info("\n")
+
+    if numbers is not None:
+        for n in numbers:
+            # get natural population analysis value corresponding to the atom number
+            charge_value = get_value_by_number(n, natural_charges)
+            logger.info(f"Natural Charge at atom {n} is {charge_value:.>3f}.")
+            logger.info("\n")
+        logger.info("\n")
+
+if __name__ == "__main__":
+    entry_point()
