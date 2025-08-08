1bc7812e01802a23ca2ae91462e13f321f66e966
ZihanL1 <283075@whut.edu.cn>
2025-08-08 13:51:04 +0800

    2

--- chemsmart/cli/mol/__init__.py
+++ chemsmart/cli/mol/__init__.py
@@ -4,9 +4,9 @@ from .mo import mo
 from .mol import (
     mol,  # to avoid potential conflict with inbuilt pymol module, we use mol instead
 )
-from .visualize import visualize
 from .movie import movie
 from .nci import nci
+from .visualize import visualize
 from .spin import spin
 
 __all__ = [
