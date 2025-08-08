baec6a6a0a9af59bc21a84768c17932ea58b60c2
ZihanL1 <283075@whut.edu.cn>
2025-08-08 14:06:43 +0800

    test

--- chemsmart/cli/mol/__init__.py
+++ chemsmart/cli/mol/__init__.py
@@ -4,11 +4,10 @@ from .mo import mo
 from .mol import (
     mol,  # to avoid potential conflict with inbuilt pymol module, we use mol instead
 )
-
-from .visualize import visualize
 from .movie import movie
 from .nci import nci
 from .spin import spin
+from .visualize import visualize
 
 __all__ = [
     "align",
