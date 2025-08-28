d00ece57cd9e3f4b046bbdb699d9b7bf3a1322ab
xinglong-zhang <xinglong.zhang@cuhk.edu.hk>
2025-08-28 15:13:12 +0800

    changed furo theme to sphinx_rtd_theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -68,7 +68,6 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "sphinx-rtd-theme"  # matches your requirements.txt
-# html_theme = "furo"  # matches your requirements.txt
+html_theme = "sphinx_rtd_theme"  # matches your requirements.txt
 html_static_path = ["_static"]  # create or remove
 html_title = f"{project} {release}"
