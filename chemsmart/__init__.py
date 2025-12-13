25a453f41b2fc5371c4cc5935a89e10c486f98fd
xinglong-zhang <xinglong.zhang@cuhk.edu.hk>
2025-12-13 20:26:13 +0800

    updated more specific exception errors

--- chemsmart/__init__.py
+++ chemsmart/__init__.py
@@ -4,5 +4,9 @@ from pathlib import Path
 try:
     _version_file = Path(__file__).with_name("VERSION")
     __version__ = _version_file.read_text(encoding="utf-8").strip()
-except Exception:  # pragma: no cover
+except (
+    FileNotFoundError,
+    PermissionError,
+    UnicodeDecodeError,
+):  # pragma: no cover
     __version__ = "0.0.0"
