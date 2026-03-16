8085fed1fdf1731d13d6e2247c04d957b20c6ace
Huiwen-Tan <h.tan1@uqconnect.edu.au>
2026-03-16 20:58:48 +0800

    fix database docstring

--- chemsmart/cli/database/database.py
+++ chemsmart/cli/database/database.py
@@ -10,9 +10,5 @@ logger = logging.getLogger(__name__)
 @click.group(name="database", cls=MyGroup)
 @click.pass_context
 def database(ctx):
-    """CLI for database operations.
-
-    This command group provides subcommands for managing the chemsmart
-    database, including assembling calculation data from output files.
-    """
+    """CLI for chemsmart database operations."""
     pass
