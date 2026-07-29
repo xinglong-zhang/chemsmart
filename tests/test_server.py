8323d2c1cc37d1e887e187d7b46fa47078655e44
Huiwen-Tan <h.tan1@uqconnect.edu.au>
2026-07-29 16:13:33 +0800

    set XTB server ENVARS to null by default when scratch is off

--- tests/test_server.py
+++ tests/test_server.py
@@ -96,7 +96,7 @@ conda activate ~/anaconda3/envs/chemsmart
         assert xtb_executable.conda_env == xtb_conda_env
         assert xtb_executable.modules is None
         assert xtb_executable.scripts is None
-        assert xtb_executable.envars == "export SCRATCH=~/scratch\n"
+        assert xtb_executable.envars is None
 
     def test_slurm_submitter_writes_extra_scheduler_directives(self):
         server = Server(
