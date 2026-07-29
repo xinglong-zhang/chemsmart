692b6f8c7e6654777aede1dd76f3c1f6da185459
Huiwen-Tan <h.tan1@uqconnect.edu.au>
2026-07-29 17:49:26 +0800

    improve XTBRoute property names and add route parser tests

--- chemsmart/jobs/xtb/job.py
+++ chemsmart/jobs/xtb/job.py
@@ -60,6 +60,10 @@ class XTBJob(Job):
     def errfile(self):
         return os.path.join(self.folder, f"{self.label}.err")
 
+    @property
+    def restartfile(self):
+        return os.path.join(self.folder, "xtbrestart")
+
     def _determine_folder(self):
         folder = os.path.abspath(os.getcwd())
         if self.label:
@@ -74,6 +78,7 @@ class XTBJob(Job):
         folder = self._create_backup_folder_name()
         self.backup_file(self.xyzfile, folder=folder, **kwargs)
         self.backup_file(self.outputfile, folder=folder, **kwargs)
+        self.backup_file(self.restartfile, folder=folder, **kwargs)
 
     def _output(self):
         if not os.path.exists(self.outputfile):
