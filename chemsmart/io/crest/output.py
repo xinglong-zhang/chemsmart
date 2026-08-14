fedca929624e9fca1c755fcbebfb25270a069e92
Huiwen-Tan <h.tan1@uqconnect.edu.au>
2026-08-14 18:01:02 +0800

    refactor folder output parser delegation into mixin

--- chemsmart/io/crest/output.py
+++ chemsmart/io/crest/output.py
@@ -5,11 +5,12 @@ from chemsmart.io.crest.file import CRESTEnergiesFile, CRESTMainOut
 from chemsmart.io.crest.folder import CRESTFolder
 from chemsmart.io.molecules.structure import Molecule
 from chemsmart.utils.constants import kcal_per_mol_to_hartree
+from chemsmart.utils.mixins import FolderOutputMixin
 
 logger = logging.getLogger(__name__)
 
 
-class CRESTOutput:
+class CRESTOutput(FolderOutputMixin):
     """
     Integrates and exposes all relevant data from a CREST calculation directory.
 
@@ -22,38 +23,14 @@ class CRESTOutput:
             CRESTFolder instance.
     """
 
+    FILE_PARSERS = (
+        "main_out",
+        "energies_file",
+    )
+
     def __init__(self, folder):
         self.folder = (
             folder if isinstance(folder, CRESTFolder) else CRESTFolder(folder)
-        )
-
-    def __getattr__(self, name):
-        """
-        Delegate attribute access to internal file parsers or cached properties.
-
-        This allows CRESTOutput to transparently expose all properties and methods
-        from CRESTMainOut, CRESTEnergiesFile, and any future parsers.
-        """
-        # List of internal parsers to search, in order of priority
-        file_parsers = [
-            "main_out",
-            "energies_file",
-        ]
-        # Try to get from each parser in order
-        for file_parser in file_parsers:
-            try:
-                parser = object.__getattribute__(self, file_parser)
-                if parser is not None:
-                    try:
-                        return getattr(parser, name)
-                    except AttributeError:
-                        # This parser doesn't have the attribute, try next one
-                        continue
-            except Exception as e:
-                logger.debug(f"Failed to access parser {file_parser}: {e}")
-                continue
-        raise AttributeError(
-            f"'{type(self).__name__}' object has no attribute '{name}'"
         )
 
     @cached_property
--- chemsmart/io/xtb/output.py
+++ chemsmart/io/xtb/output.py
@@ -17,12 +17,13 @@ from chemsmart.io.xtb.file import (
 from chemsmart.io.xtb.folder import XTBFolder
 from chemsmart.io.xyz.xyzfile import XYZFile
 from chemsmart.utils.io import create_molecule_list
+from chemsmart.utils.mixins import FolderOutputMixin
 from chemsmart.utils.utils import string2index_1based
 
 logger = logging.getLogger(__name__)
 
 
-class XTBOutput:
+class XTBOutput(FolderOutputMixin):
     """
     Integrates and exposes all relevant data from an xTB calculation directory.
 
@@ -35,49 +36,21 @@ class XTBOutput:
         folder (str or XTBFolder): Path to xTB calculation folder or XTBFolder instance.
     """
 
+    FILE_PARSERS = (
+        "main_out",
+        "charges_file",
+        "energy_file",
+        "engrad_file",
+        "g98_file",
+        "gradient_file",
+        "hessian_file",
+        "vibspectrum_file",
+        "wbo_file",
+    )
+
     def __init__(self, folder):
-        """
-        Initialize XTBOutput with a folder path or XTBFolder instance.
-        """
         self.folder = (
             folder if isinstance(folder, XTBFolder) else XTBFolder(folder)
-        )
-
-    def __getattr__(self, name):
-        """
-        Delegate attribute access to internal file parsers or cached properties.
-
-        This allows XTBOutput to transparently expose all properties and methods
-        from XTBMainOut, XTBEngradFile, XTBChargesFile, XTBWibergBondOrderFile,
-        XTBHessianFile, XTBGradientFile, XTBVibSpectrumFile, and any future parsers.
-        """
-        # List of internal parsers to search, in order of priority
-        file_parsers = [
-            "main_out",
-            "charges_file",
-            "energy_file",
-            "engrad_file",
-            "g98_file",
-            "gradient_file",
-            "hessian_file",
-            "vibspectrum_file",
-            "wbo_file",
-        ]
-        # Try to get from each parser in order
-        for file_parser in file_parsers:
-            try:
-                parser = object.__getattribute__(self, file_parser)
-                if parser is not None:
-                    try:
-                        return getattr(parser, name)
-                    except AttributeError:
-                        # This parser doesn't have the attribute, try next one
-                        continue
-            except Exception as e:
-                logger.debug(f"Failed to access parser {file_parser}: {e}")
-                continue
-        raise AttributeError(
-            f"'{type(self).__name__}' object has no attribute '{name}'"
         )
 
     @cached_property
